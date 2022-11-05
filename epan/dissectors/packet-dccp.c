f633c875b0c2a66acde858dcb135fff7254e79f5
GREGORIO-M <113447916+GREGORIO-M@users.noreply.github.com>
2022-11-05 17:51:38 +0000

    Update packet-dccp.c

--- epan/dissectors/packet-dccp.c
+++ epan/dissectors/packet-dccp.c
@@ -988,21 +988,19 @@ dissect_options(tvbuff_t *tvb, packet_info *pinfo,
                     } else {
                         mp_option_sub_item = proto_tree_add_item(option_tree, hf_mpdccp_removeaddr, tvb, offset, option_len, ENC_BIG_ENDIAN);
                         expert_add_info_format(pinfo, mp_option_sub_item, &ei_dccp_option_len_bad,
-                                   "Wrong Data checksum length, [%u != 4]", option_len);
+                                   "Wrong Data checksum length, [%u != 1]", option_len);
                     }
                     //offset+=1;//
                     break;
                 case 9:
-                    mp_option_sub_item = proto_tree_add_item(option_tree, hf_mpdccp_prio_value, tvb, offset+1, 1, ENC_BIG_ENDIAN);//+1?
-                    mp_option_sub_tree = proto_item_add_subtree(mp_option_sub_item, ett_dccp_options_item);
-                    offset += 1;
                     if (option_len == 1) {
-                        proto_tree_add_item(mp_option_sub_tree, hf_mpdccp_prio_value, tvb, offset, 1, ENC_BIG_ENDIAN);
+                        proto_tree_add_item(option_tree, hf_mpdccp_prio_value, tvb, offset+1, 1, ENC_BIG_ENDIAN);
                     } else {
                         mp_option_sub_item = proto_tree_add_item(mp_option_sub_tree, hf_dccp_option_data, tvb, offset, option_len, ENC_NA);
                         expert_add_info_format(pinfo, mp_option_sub_item, &ei_dccp_option_len_bad,
                                    "Wrong Data checksum length, [%u != 1]", option_len);
                     }
+                    offset += 1;//Use at least once
                     break;
                 case 10:
                     mp_option_sub_item = proto_tree_add_item(option_tree, hf_mpdccp_close,tvb, offset+1, option_len, ENC_BIG_ENDIAN);
